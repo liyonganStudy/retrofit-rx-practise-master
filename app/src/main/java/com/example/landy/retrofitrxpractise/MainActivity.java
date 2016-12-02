ba2c4cfd257b395a05e7130745d0e701b155a451
landy <liyonganStudy@163.com>
2016-12-02 12:06:06 +0800

    添加base－library

--- app/src/main/java/com/example/landy/retrofitrxpractise/MainActivity.java
+++ app/src/main/java/com/example/landy/retrofitrxpractise/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.landy.retrofitrxpractise;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
